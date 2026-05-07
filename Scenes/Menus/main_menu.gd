a71eeb855d263de446f685d7fd85eb89314f76ed
Branko Van Tendeloo <brankovt1bvt@gmail.com>
2026-05-07 19:28:46 +0200

    Main menu

---
+++ Scenes/Menus/main_menu.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@onready var BowlingButton = $Sports/BowlingButton
+@onready var QuitButton = $Sports/QuitButton
+
+func _ready() -> void:
+	BowlingButton.grab_focus()
+
+
+### Button presses
+func _on_bowling_button_pressed() -> void:
+	print("LOADING BOWLING SCENE")
+	get_tree().change_scene_to_file("res://Scenes/3D/bowling.tscn")
+
+func _on_quit_button_pressed() -> void:
+	print("QUITTING PROGRAM...")
+	get_tree().quit()
+
+
+### Button focus grabbers.
+func _on_bowling_button_mouse_entered() -> void:
+	BowlingButton.grab_focus()
+
+func _on_quit_button_mouse_entered() -> void:
+	QuitButton.grab_focus()
