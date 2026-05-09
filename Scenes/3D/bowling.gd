056c6005b8c55acc6bc9fbe76aefb90f8b30e4db
Branko Van Tendeloo <brankovt1bvt@gmail.com>
2026-05-09 23:12:30 +0200

    Added pin reset code

--- Scenes/3D/bowling.gd
+++ Scenes/3D/bowling.gd
@@ -1,5 +1,34 @@
 extends Node3D
 
-func _process(delta: float) -> void:
+@onready var ball_body = $Ball
+@onready var pins := {}
+
+var ball_finished: bool = false
+
+func _ready() -> void:
+	for pin: Node3D in $Pins.get_children():
+		pins[pin] = {"standing": true, "in_play": true}
+		pins[pin]["position"] = pin.position
+		pins[pin]["rotation"] = pin.rotation
+	reset_pins()
+
+func _process(_delta: float) -> void:
 	if Input.is_action_just_released("ui_accept"):
-		get_tree().reload_current_scene()
+		reset_pins()
+
+## Resets all pins to their original state
+func reset_pins() -> void:
+	for pin in pins.keys():
+		pins[pin]["standing"] = true
+		pins[pin]["in_play"] = true
+		reset_pin_position(pin)
+		pin.freeze = true
+	for pin in pins.keys():
+		pin.freeze = false
+
+## Resets a pin's position and velocity
+func reset_pin_position(pin: RigidBody3D) -> void:
+	pin.linear_velocity = Vector3.ZERO
+	pin.angular_velocity = Vector3.ZERO
+	pin.position = pins[pin]["position"]
+	pin.rotation = pins[pin]["rotation"]
